05040f70bffab6896fbcb020a21f48c1c0b504d3
Srivasu215 <sri.vasu215@gmail.com>
2025-04-26 15:45:09 +0530

    Bugs fixed

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 const app = express()
 const port = 3000;
+import bodyparser from "body-parser";
 import { router as routerFromTasks } from "./Tasks/routes.js";
 import { router as routerFromFiles } from "./Files/routes.js";
+app.use(bodyparser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
