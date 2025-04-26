facdbd7584b01a1f9374923b4939f3b059c335ee
sharath0036 <ksharathbabu2@gmail.com>
2025-04-26 13:28:32 +0530

    Read File

--- Files/routes.js
+++ Files/routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 import { router as routerFromCreate } from "./Create/routes.js";
 import { router as routerFromDelete } from "./Delete/routes.js";
+import { router as routerFromRead} from "./Read/routes.js";
 
 router.use("/Create", routerFromCreate);
 router.use("/Delete", routerFromDelete);
+router.use("/Read", routerFromRead);
 
 export {
     router
