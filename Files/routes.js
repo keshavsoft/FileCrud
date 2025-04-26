22e99efdef33ba55e25939ba4b20b288698d6d9e
keshavsoft <nknnkumar@yahoo.com>
2025-04-26 13:03:19 +0530

    Delete to write

--- Files/routes.js
+++ Files/routes.js
@@ -2,8 +2,10 @@ import express from 'express';
 const router = express.Router();
 
 import { router as routerFromCreate } from "./Create/routes.js";
+import { router as routerFromDelete } from "./Delete/routes.js";
 
-router.use("/Create", routerFromCreate);;
+router.use("/Create", routerFromCreate);
+router.use("/Delete", routerFromDelete);
 
 export {
     router
