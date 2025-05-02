f2dc422e553bc362d258d3d2e1a30de0ff763d7a
ShaikAmeenaH <dcrpm2051m@gmail.com>
2025-05-02 18:51:43 +0530

    alter delete updated

--- Tasks/routes.js
+++ Tasks/routes.js
@@ -3,9 +3,13 @@ const router = express.Router();
 
 import { router as routerFromCreate} from "./Insert/routes.js";
 import { router as routerFromRead} from "./Read/routes.js";
+import { router as routerFromAlter} from "./Alter/routes.js";
+import { router as routerFromDelete} from "./Delete/routes.js";
 
 router.use("/Insert", routerFromCreate);
 router.use("/Read", routerFromRead);
+router.use("/Alter", routerFromAlter);
+router.use("/Delete", routerFromDelete);
 
 export {
     router
