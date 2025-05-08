15afc77c12decb33627f13f85c852cfc1e512be3
sharath0036 <ksharathbabu2@gmail.com>
2025-05-08 13:19:36 +0530

    AlterWithPk

--- FileAsArrayContent/routes.js
+++ FileAsArrayContent/routes.js
@@ -11,6 +11,7 @@ import { router as routerFromInsertWithPkAndUuId } from "./InsertWithPkAndUuId/r
 import { router as routerFromRowDataWithPk } from "./RowDataWithPk/routes.js";
 import { router as routerFromRowDataWithUuId } from "./RowDataWithUuId/routes.js";
 import { router as routerFromInsertWithPkAndUuIdAndDateTime } from "./InsertWithPkAndUuIdAndDateTime/routes.js";
+import { router as AlterWithPk } from "./AlterWithPk/routes.js";
 
 router.use("/Alter", routerFromAlter);
 router.use("/Insert", routerFromInsert);
@@ -22,6 +23,7 @@ router.use("/InsertWithPkAndUuId", routerFromInsertWithPkAndUuId);
 router.use("/RowDataWithPk", routerFromRowDataWithPk);
 router.use("/RowDataWithUuId", routerFromRowDataWithUuId);
 router.use("/InsertWithPkAndUuIdAndDateTime", routerFromInsertWithPkAndUuIdAndDateTime);
+router.use("/AlterWithPk", AlterWithPk);
 
 export {
     router
